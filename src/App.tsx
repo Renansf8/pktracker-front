d84a83368a1ce2cf5a971b2e48539f1284481029
Renan Santiago <renansferreira8@gmail.com>
2025-06-19 11:35:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { SignIn } from "./pages/signin";
+
+function App() {
+  return <SignIn />;
+}
+
+export default App;
---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function SignIn() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Card className="mx-auto max-w-sm w-full bg-background text-text border-input-border">
+        <CardHeader>
+          <CardTitle>Faça login para continuar</CardTitle>
+          <CardDescription className="text-text-secondary">
+            Insira seu email e senha para continuar
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  className="border-input-border"
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm text-text-secondary"
+                  >
+                    Esqueceu sua senha?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  className="border-input-border"
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full p-0 bg-black">
+                  Login
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm font-bold">
+              Não tem uma conta?{" "}
+              <a href="#" className=" text-text-secondary">
+                Criar conta
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
